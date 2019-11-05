ad65fe8dc09137c0dc88baeec09a659b8f1570c5
Nick Fox <nick@foxsec.net>
2019-11-04 23:28:02 -0500

    Add net-tools

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S git python python-pip python2 python2-pip lib32-gcc-libs clang llvm \
 pacman-contrib go base-devel vim tmux unzip zip unrar wget mlocate cmake python2-virtualenv \
-netcat --noconfirm
+netcat net-tools --noconfirm
 
 echo "set -g mouse on" > ~/.tmux.conf
 mkdir ~/tools
