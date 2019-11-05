ef741f3036f0b05cf32e6128c908957ee71b8df9
Nick Fox <nick@foxsec.net>
2019-11-04 21:08:39 -0500

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,40 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.ssh.username = "vagrant"
+  config.ssh.forward_agent = true
+  config.vm.provision :shell, :path => "vagrant_setup.sh", :privileged => false
+
+  name = "pwnvm-arch"
+  memory = "2048"
+
+  config.vm.define "pwnvm-arch", primary: true do |a64|
+    a64.vm.box = "archlinux/archlinux"
+    a64.vm.provider "virtualbox" do |vb, override|
+      override.vm.network "private_network", ip: "10.10.10.10"
+      # Sync a folder between the host and all guests.
+      # Uncomment this line (and adjust as you like)
+      override.vm.synced_folder "~/ctf", "/ctf"
+
+      vb.name = name
+      vb.memory = memory
+      vb.gui = false
+    end
+    a64.vm.provider "libvirt" do |lv, override|
+      override.vm.network "private_network",
+        :libvirt__network_name => "trusted", # modify to fit your virtual network setup
+        :libvirt__netmask => "255.255.255.0"
+      # Sync a folder between the host and all guests.
+      # Uncomment this line (and adjust as you like)
+      # NOTE: Requires installation of the nfs-server package on the host machine
+      # If `vagrant up` hangs at Mounting NFS folders, modify your firewall configuration
+      # to allow nfs, rpc, and mountd services
+      override.vm.synced_folder "~/ctf", "/ctf", :nfs => true
+      override.vm.hostname = name
+      lv.memory = memory
+    end
+  end
+end
---
+++ vagrant_setup.sh
@@ -0,0 +1,60 @@
+# update and pull basic core development packages/utilities
+sudo pacman -Syu --noconfirm
+sudo pacman -S git python python-pip python2 python2-pip lib32-gcc-libs clang llvm \
+pacman-contrib go base-devel vim tmux unzip zip unrar wget mlocate cmake --noconfirm
+
+echo "set -g mouse on" > ~/.tmux.conf
+
+# yay for AUR packages
+mkdir aur && cd aur
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si --noconfirm
+cd ~
+
+# gef and dependencies
+yay -S python-capstone python-keystone python-unicorn python-ropper ropgadget gef-git --noconfirm
+# Note if you want to use peda or pwndbg instead, you need to manually modify your ~/.gdbinit
+echo "source /usr/share/gef/gef.py" > ~/.gdbinit
+
+# pwndbg
+sudo pacman -S pwndbg --noconfirm
+echo "#source /usr/share/pwndbg/gdbinit.py" >> ~/.gdbinit
+
+# peda
+sudo pacman -S peda --noconfirm
+echo "#source /usr/share/peda/peda.py" >> ~/.gdbinit
+
+# pwntools
+yay -S python-pwntools-git --noconfirm
+
+# pwntools for python2
+# The python2-pwntools package conflicts with the Python 3 version so install into a venv in ~/tools
+cd ~/tools && virtualenv2 pwntools2 && source pwntools2/bin/activate
+pip install pwntools
+deactivate
+
+# qemu
+sudo pacman -S qemu-headless qemu-headless-arch-extra --noconfirm
+
+# r2
+sudo pacman -S radare2 --noconfirm
+
+# angr
+# The python-angr-git AUR package requires gcc7 being built from source (which takes ages)
+# and also tramples a bunch of other dependencies like z3, so we're just going to use a 
+# venv to install it instead
+cd ~/tools && python -m venv angr && source angr/bin/activate
+pip install angr
+deactivate
+
+# binwalk
+sudo pacman -S binwalk --noconfirm
+
+# fixenv
+sudo wget -O /usr/local/bin/fixenv https://raw.githubusercontent.com/hellman/fixenv/master/r.sh
+sudo chmod +x /usr/local/bin/fixenv
+
+# z3
+sudo pacman -S python-z3 --noconfirm
+
+rm -rf ~/aur
