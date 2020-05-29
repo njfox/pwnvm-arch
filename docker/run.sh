6c0369c50df2349abd086c7ea34a5f7f92313bf5
Nick Fox <nick@foxsec.net>
2020-05-29 15:31:17 -0400

    add apparmor security-opt for osx

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$@" njfox/pwnvm-arch /bin/bash
+docker run --rm -it --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" --security-opt="apparmor=unconfined" "$@" njfox/pwnvm-arch /bin/bash
